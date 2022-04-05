8b5448330d15f893e8a2782a3c5eb1bd47df25c5
Javier <javier.zen@gmail.com>
2022-04-05 22:02:26 +0200

    update 1

---
+++ script.js
@@ -0,0 +1,12 @@
+window.VIDEOASK_EMBED_CONFIG = {
+  "kind": "widget",
+  "url": "https://www.videoask.com/fn5pz0q1v",
+  "options": {
+    "widgetType": "VideoThumbnailExtraLarge",
+    "text": "Talk to me",
+    "backgroundColor": "#E26D5A",
+    "position": "bottom-right",
+    "dismissible": true
+  }
+}
+  
